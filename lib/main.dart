428c0302cdef58a66b83fec953f0ef934c49e9e9
seijiro <modeverv@gmail.com>
2021-12-08 22:00:14 +0900

    2021-12-08 22:00:14

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,62 @@ class Myapp extends StatelessWidget {
                   fontFamily: 'source',
                   letterSpacing: 2.5,
                 ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 50.0,
+                ),
+                padding: EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 12.5,
+                    ),
+                    Text(
+                      '+81 123 456 789',
+                      style: TextStyle(
+                        fontFamily: 'source',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 50.0,
+                ),
+                padding: EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 12.5,
+                    ),
+                    Text(
+                      'modeverv@example.com',
+                      style: TextStyle(
+                        fontFamily: 'source',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
