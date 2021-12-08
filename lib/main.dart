8bd2996d2572801c466a10cc0db29118dd4a6d39
seijiro <modeverv@gmail.com>
2021-12-08 21:08:39 +0900

    2021-12-08 21:08:37

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,25 @@ class Myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: SafeArea(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  width: 100.0,
-                  color: Colors.red,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                        width: 100.0, height: 100.0, color: Colors.yellow),
-                    Container(width: 100.0, height: 100.0, color: Colors.grey)
-                  ],
-                ),
-                Container(width: 100.0, color: Colors.blue),
-              ],
-            ),
-          )),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 100.0,
+                foregroundImage: AssetImage('images/me.jpg'),
+              ),
+              Text(
+                'Test Example',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
